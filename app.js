a30a0de28cc5d838947b93adb009c2522f600c29
Jonas Gehrke <jonasegherke@gmail.com>
2022-06-28 13:52:35 +0200

    trying larger json parseer

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 
 const app = express();
 
-
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 import speckleRouter from "./routers/speckleRouter.js"
 app.use(speckleRouter);
